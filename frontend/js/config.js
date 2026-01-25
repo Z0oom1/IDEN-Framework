e93834dbde9d6a2f8175d16efb0c20d0575d5464
Caio Rod <77243616+Z0oom1@users.noreply.github.com>
2026-01-25 02:04:18 -0300

    Update config.js

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,8 +1,3 @@
-
-const SERVER_IP = "192.168.2.106"; 
+const SERVER_IP = "localhost"; // Em vez de 192.168.2.106
 const SERVER_PORT = "2006";
-
-
 const API_BASE_URL = `http://${SERVER_IP}:${SERVER_PORT}`;
-
-console.log("🎯 Sistema configurado para conectar em:", API_BASE_URL);
